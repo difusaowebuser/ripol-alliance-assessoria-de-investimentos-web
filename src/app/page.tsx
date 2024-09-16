5631e86b973e9da15a7520a773466db238170c63
Wiatagan Paz - Difusão Web <wiatagan@difusaoweb.com>
2024-09-16 18:21:35 -0300

    feat: section solutions will be updated.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -313,11 +313,42 @@ export default function Home() {
             </div>
           </div>
         </section>
-        <section className="bg-[url('/section-solutions-background.png')] bg-container bg-no-repeat bg-right my-20">
-          <h2>
-            Soluções <span className="font-bold">simples</span> para situações
-            complexas
-          </h2>
+        <section className="bg-[url('/section-solutions-background.png')] bg-container bg-no-repeat bg-right my-20 py-20">
+          <div className="container mx-auto">
+            <h2 className="text-5xl lg:text-6xl xl:text-7xl text-white mb-8">
+              Soluções <span className="font-bold">simples</span> para situações
+              complexas
+            </h2>
+            <div className="grid grid-cols-1 gap-6 lg:grid-cols-3 lg:gap-20 lg:pl-20">
+              <div className="pl-4 py-1 border-l-[10px] border-dotted">
+                <p>
+                  Lorem ipsum odor amet, consectetuer adipiscing elit. Mattis
+                  ante pharetra et justo semper vehicula dui a. Dictumst mollis
+                  aliquam ultricies vel massa conubia viverra urna. Semper mus
+                  class dictumst placerat viverra sed gravida semper. Himenaeos
+                  natoque blandit cubilia integer arcu nostra in proin tempus.
+                </p>
+              </div>
+              <div className="pl-4 py-1 border-l-[10px] border-dotted">
+                <p>
+                  Lorem ipsum odor amet, consectetuer adipiscing elit. Mattis
+                  ante pharetra et justo semper vehicula dui a. Dictumst mollis
+                  aliquam ultricies vel massa conubia viverra urna. Semper mus
+                  class dictumst placerat viverra sed gravida semper. Himenaeos
+                  natoque blandit cubilia integer arcu nostra in proin tempus.
+                </p>
+              </div>
+              <div className="pl-4 py-1 border-l-[10px] border-dotted">
+                <p>
+                  Lorem ipsum odor amet, consectetuer adipiscing elit. Mattis
+                  ante pharetra et justo semper vehicula dui a. Dictumst mollis
+                  aliquam ultricies vel massa conubia viverra urna. Semper mus
+                  class dictumst placerat viverra sed gravida semper. Himenaeos
+                  natoque blandit cubilia integer arcu nostra in proin tempus.
+                </p>
+              </div>
+            </div>
+          </div>
         </section>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
