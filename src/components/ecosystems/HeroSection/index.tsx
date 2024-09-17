d97afc619aeb3e2bba11f13b34302a9502c39e98
Wiatagan Paz - Difusão Web <wiatagan@difusaoweb.com>
2024-09-17 18:13:31 -0300

    fix: Hero section will be updated.

--- src/components/ecosystems/HeroSection/index.tsx
+++ src/components/ecosystems/HeroSection/index.tsx
@@ -1,6 +1,5 @@
 "use client";
 import * as React from "react";
-import Image from "next/image";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 
