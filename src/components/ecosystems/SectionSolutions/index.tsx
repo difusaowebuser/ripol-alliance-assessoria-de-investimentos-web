c8fdba39fc03c3d720d6c042db942401b6344c04
Wiatagan Paz - Difusão Web <wiatagan@difusaoweb.com>
2024-10-01 20:13:44 -0300

    refactor: solutions section will be updated.

--- src/components/ecosystems/SectionSolutions/index.tsx
+++ src/components/ecosystems/SectionSolutions/index.tsx
@@ -1,42 +1,28 @@
 import * as React from "react";
 
+const soluctions = [
+  "Um parceiro comprometido com a segurança, a continuidade e o crescimento do seu legado familiar.",
+  "Profissionais multidisciplinares, com o olhar voltado para cada negócio e para cada propósito.",
+  "Os projetos são construídos com total sigilo, preservando sempre a imagem e a reputação das famílias.",
+];
+
 export const SectionSolutions = () => {
   return (
     <>
       <section className="bg-[url('/section-solutions-background.png')] bg-container bg-no-repeat bg-right my-20 py-20">
         <div className="container mx-auto">
           <h2 className="text-5xl lg:text-6xl xl:text-7xl text-white mb-8">
-            Soluções <span className="font-bold">simples</span> para situações
-            complexas
+            Entendemos a realidade de nossos clientes.
           </h2>
           <div className="grid grid-cols-1 gap-6 lg:grid-cols-3 lg:gap-20 lg:pl-20">
-            <div className="pl-4 py-1 border-l-[10px] border-dotted">
-              <p>
-                Lorem ipsum odor amet, consectetuer adipiscing elit. Mattis ante
-                pharetra et justo semper vehicula dui a. Dictumst mollis aliquam
-                ultricies vel massa conubia viverra urna. Semper mus class
-                dictumst placerat viverra sed gravida semper. Himenaeos natoque
-                blandit cubilia integer arcu nostra in proin tempus.
-              </p>
-            </div>
-            <div className="pl-4 py-1 border-l-[10px] border-dotted">
-              <p>
-                Lorem ipsum odor amet, consectetuer adipiscing elit. Mattis ante
-                pharetra et justo semper vehicula dui a. Dictumst mollis aliquam
-                ultricies vel massa conubia viverra urna. Semper mus class
-                dictumst placerat viverra sed gravida semper. Himenaeos natoque
-                blandit cubilia integer arcu nostra in proin tempus.
-              </p>
-            </div>
-            <div className="pl-4 py-1 border-l-[10px] border-dotted">
-              <p>
-                Lorem ipsum odor amet, consectetuer adipiscing elit. Mattis ante
-                pharetra et justo semper vehicula dui a. Dictumst mollis aliquam
-                ultricies vel massa conubia viverra urna. Semper mus class
-                dictumst placerat viverra sed gravida semper. Himenaeos natoque
-                blandit cubilia integer arcu nostra in proin tempus.
-              </p>
-            </div>
+            {soluctions.map((service, index) => (
+              <div
+                className="pl-4 py-1 border-l-[10px] border-dotted"
+                key={`p-${index}`}
+              >
+                <p>{service}</p>
+              </div>
+            ))}
           </div>
         </div>
       </section>
